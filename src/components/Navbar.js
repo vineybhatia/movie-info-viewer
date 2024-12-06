fa5f0a075233c81cb6366ff7e367ea7e4f91f625
Viney Bhatia <bhatia.viney@gmail.com>
2024-12-06 10:33:53 -0500

    fix: nav to use Link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
+import { Link } from "react-router-dom"; // Import Link
 
 const NavigationBar = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
       <Container>
-        <Navbar.Brand href="/">🎥 MovieInfo</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">🎥 MovieInfo</Navbar.Brand> {/* Use 'as={Link}' */}
         <Navbar.Toggle aria-controls="navbar-nav" />
         <Navbar.Collapse id="navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link as={Link} to="/">Home</Nav.Link> {/* Use 'as={Link}' */}
+            <Nav.Link as={Link} to="/about">About</Nav.Link> {/* Use 'as={Link}' */}
           </Nav>
         </Navbar.Collapse>
       </Container>
